e903f731cd51e4edd2497a0321bfe6662be22eef
ubeyidah <ubeyidah@gamil.com>
2025-06-05 14:58:54 +0300

    create main entry point for the project cli interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        welcomeText();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            try{
+                System.out.print("Choose: ");
+                int choice = scanner.nextInt();
+                switch (choice){
+                    case 1:
+                        System.out.println("account creating....");
+                        break;
+                    case 2:
+                        System.out.println("login....");
+                        break;
+                    case 3:
+                        System.out.println("Thanks for using the bank.");
+                        return;
+                    default:
+                        System.out.println("Invalid choice.");
+
+                }
+
+            }catch (Exception err){
+                System.out.println("Invalid choice. only integer is accepted!");
+            }
+
+
+        }
+    }
+
+    public static void welcomeText() {
+        System.out.println("\n--- Welcome to Simple Bank System ---");
+        System.out.println("1. Create Account");
+        System.out.println("2. Login");
+        System.out.println("3. Exit");
     }
 }
